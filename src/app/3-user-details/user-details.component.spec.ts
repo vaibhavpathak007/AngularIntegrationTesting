77edf3609c51f47e2dc5d4b8631d077e570d0803
vaibhavpathak007 <vaibhavpathak007@gmail.com>
2019-05-04 15:08:55 +0530

    providing stub for router in onInit()

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -4,14 +4,30 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub{
+  navigate(params){ // implemented only one method which is used in component.
+
+  }
+}
+
+class ActivatedRouteStub{
+  params: Observable<any> = of(); // to avoid error of subscribe of undefined in onInit()
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
     })
     .compileComponents();
   }));
@@ -23,6 +39,9 @@ xdescribe('UserDetailsComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); 
+    // for component creation we need to have all fake dependency injected.
+    // use of original router dependency will decrease performance. so we use sub (fake service)
   });
+
 });
